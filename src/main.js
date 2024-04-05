5699d73bd809889f38a85ab6b16f7743e19ba41d
faran-ahmed1 <farantoor313@gmail.com>
2024-04-06 03:22:20 +0500

    remove toast

--- src/main.js
+++ src/main.js
@@ -4,12 +4,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import baseButton from "./components/UI/baseButton.vue";
-import Toast from "./components/Toast.vue";
 
 const app = createApp(App);
 
 app.component("base-button", baseButton);
-app.component("Toast", Toast);
 app.use(router);
 app.use(store);
 app.mount("#app");
